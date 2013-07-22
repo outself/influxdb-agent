21c731f709fca2be11525199b5c358969b130eaa
John Shahid <jvshahid@gmail.com>
2013-07-22 15:09:40 -0400

    add cpu stats.

--- src/apps/agent/agent.go
+++ src/apps/agent/agent.go
@@ -23,10 +23,20 @@ func main() {
 	ep := errplane.New("w.apiv3.errplane.com", "udp.apiv3.errplane.com", "app4you2love", "staging", "962cdc9b-15e7-4b25-9a0d-24a45cfc6bc1")
 	ch := make(chan error)
 	go memStats(ep, ch)
-	fmt.Printf("Server started successfully\n")
+	go cpuStats(ep, ch)
+	fmt.Printf("Agent started successfully\n")
 	err := <-ch
 	fmt.Printf("Data collection stopped unexpectedly. Error: %s\n", err)
 	return
+}
+
+func report(ep *errplane.Errplane, metric string, value float64, timestamp time.Time, dimensions errplane.Dimensions, ch chan error) bool {
+	err := ep.Report(metric, value, timestamp, "", dimensions)
+	if err != nil {
+		ch <- err
+		return true
+	}
+	return false
 }
 
 func memStats(ep *errplane.Errplane, ch chan error) {
@@ -34,23 +44,68 @@ func memStats(ep *errplane.Errplane, ch chan error) {
 	swap := sigar.Swap{}
 
 	for {
-		mem.Get()
-		swap.Get()
+		err := mem.Get()
+		if err != nil {
+			ch <- err
+			return
+		}
+		err = swap.Get()
+		if err != nil {
+			ch <- err
+			return
+		}
 
-		err := ep.Report("server.stats.memory.free", float64(mem.Free), time.Now(), "", errplane.Dimensions{
-			"host": hostname,
-		})
+		dimensions := errplane.Dimensions{"host": hostname}
+		timestamp := time.Now()
+
+		if report(ep, "server.stats.memory.free", float64(mem.Free), timestamp, dimensions, ch) ||
+			report(ep, "server.stats.memory.used", float64(mem.Used), timestamp, dimensions, ch) {
+			return
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
+
+func cpuStats(ep *errplane.Errplane, ch chan error) {
+	skipFirst := true
+
+	prevCpu := sigar.Cpu{}
+	cpu := sigar.Cpu{}
+
+	for {
+		timestamp := time.Now()
+		err := cpu.Get()
 		if err != nil {
 			ch <- err
 			return
 		}
-		err = ep.Report("server.stats.memory.used", float64(mem.Used), time.Now(), "", errplane.Dimensions{
-			"host": hostname,
-		})
-		if err != nil {
-			ch <- err
-			return
+
+		if !skipFirst {
+			dimensions := errplane.Dimensions{"host": hostname}
+
+			total := float64(cpu.Total() - prevCpu.Total())
+
+			sys := float64(cpu.Sys-prevCpu.Sys) / total * 100
+			user := float64(cpu.User-prevCpu.User) / total * 100
+			idle := float64(cpu.Idle-prevCpu.Idle) / total * 100
+			wait := float64(cpu.Wait-prevCpu.Wait) / total * 100
+			irq := float64(cpu.Irq-prevCpu.Irq) / total * 100
+			softirq := float64(cpu.SoftIrq-prevCpu.SoftIrq) / total * 100
+			stolen := float64(cpu.Stolen-prevCpu.Stolen) / total * 100
+
+			if report(ep, "server.stats.cpu.sys", sys, timestamp, dimensions, ch) ||
+				report(ep, "server.stats.cpu.user", user, timestamp, dimensions, ch) ||
+				report(ep, "server.stats.cpu.idle", idle, timestamp, dimensions, ch) ||
+				report(ep, "server.stats.cpu.wait", wait, timestamp, dimensions, ch) ||
+				report(ep, "server.stats.cpu.irq", irq, timestamp, dimensions, ch) ||
+				report(ep, "server.stats.cpu.softirq", softirq, timestamp, dimensions, ch) ||
+				report(ep, "server.stats.cpu.stolen", stolen, timestamp, dimensions, ch) {
+				return
+			}
 		}
+		skipFirst = false
+		prevCpu = cpu
 		time.Sleep(10 * time.Second)
 	}
 }
