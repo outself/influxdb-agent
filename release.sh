22d842bf80923d228f1165e96e529fd845cabf0e
John Shahid <jvshahid@gmail.com>
2013-07-24 13:32:24 -0400

    fix a typo.

--- release.sh
+++ release.sh
@@ -31,7 +31,7 @@ git push origin $current_branch
 git push origin --tags
 
 for filepath in out_rpm/*.{rpm,deb}; do
-    [ -e "$i" ] || continue
+    [ -e "$filepath" ] || continue
     filename=`dirname $filepath`
     AWS_CONFIG_FILE=~/aws.conf aws s3 put-object --bucket errplane-agent --key $filename --body $filepath
 done
