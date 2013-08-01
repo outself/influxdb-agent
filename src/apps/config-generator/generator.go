3c26033a8dcfc4928cf25b0cc703ca107661d397
John Shahid <jvshahid@gmail.com>
2013-08-01 18:44:44 -0400

    fix log file path.

--- src/apps/config-generator/generator.go
+++ src/apps/config-generator/generator.go
@@ -50,7 +50,7 @@ udp-port: :8127					# the udp port on which the aggregator will listen
 
 sleep: 10s                                    # frequency of sampling (accepted suffix, s for seconds, m for minutes and h for hours)
 proxy:                                        # proxy to use when making http requests (e.g. https://201.20.177.185:8080/)
-log-file: /tmp/log.txt # the log file of the agent
+log-file: /data/errplane-agent/shared/log.txt # the log file of the agent
 log-level: debug                              # debug, info, warn, error
 top-n-processes: 10                           # For processes stats the agent will report the top n processes (by memory and cpu usage)
 
