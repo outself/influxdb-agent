4223f27341b141d54a486d16a2b2b8a1b8a6ce54
John Shahid <jvshahid@gmail.com>
2013-08-07 10:10:54 -0400

    use the c.apiv3 instead of my local machine.

--- set_config.sh
+++ set_config.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-curl -v -X POST 'http://localhost:8080/databases/app4you2lovestaging/agent/thoth/configuration?api_key=962cdc9b-15e7-4b25-9a0d-24a45cfc6bc1' --data @- <<EOF
+curl -v -X POST 'http://c.apiv3.errplane.com/databases/app4you2lovestaging/agent/thoth/configuration?api_key=962cdc9b-15e7-4b25-9a0d-24a45cfc6bc1' --data @- <<EOF
 {
  "plugins":{"redis":[]},
  "processes": [
