bddb38c1a761147b4ff235698d1377bbaddadafd
John Shahid <jvshahid@gmail.com>
2013-09-05 13:34:34 -0400

    add local datastore to the agent.

--- build.sh
+++ build.sh
@@ -2,9 +2,37 @@
 
 . exports.sh
 
+leveldb_version=1.12.0
+snappy_version=1.1.0
+
 build_args=""
 if [ "$UPDATE" = "on" ]; then
     build_args="-u"
+fi
+
+snappy_dir=/tmp/snappy
+snappy_file=snappy-$snappy_version.tar.gz
+if [ ! -d $snappy_dir -o ! -e $snappy_dir/$snappy_file -o ! -e $snappy_dir/.libs/libsnappy.a ]; then
+    rm -rf $snappy_dir
+    mkdir -p $snappy_dir
+    pushd $snappy_dir
+    wget https://snappy.googlecode.com/files/$snappy_file
+    tar --strip-components=1 -xvzf $snappy_file
+    ./configure
+    make
+    popd
+fi
+
+leveldb_dir=/tmp/leveldb
+leveldb_file=leveldb-$leveldb_version.tar.gz
+if [ ! -d $leveldb_dir -o ! -e $leveldb_dir/$leveldb_file -o ! -e $leveldb_dir/libleveldb.a ]; then
+    rm -rf $leveldb_dir
+    mkdir -p $leveldb_dir
+    pushd $leveldb_dir
+    wget https://leveldb.googlecode.com/files/$leveldb_file
+    tar --strip-components=1 -xvzf $leveldb_file
+    CXXFLAGS="-I/tmp/snappy" LDFLAGS="-L/tmp/snappy/.libs" make
+    popd
 fi
 
 git submodule update --init
@@ -15,7 +43,12 @@ go get $build_args github.com/errplane/errplane-go \
     code.google.com/p/log4go \
     github.com/bmizerany/pat \
 	  github.com/pmylund/go-cache \
-    github.com/howeyc/fsnotify
+    github.com/howeyc/fsnotify \
+    code.google.com/p/goprotobuf/proto \
+    code.google.com/p/goprotobuf/protoc-gen-go
+
+rm src/datasotre/*.pb.go
+PATH=bin:$PATH protoc --go_out=. src/datastore/*.proto
 
 go build apps/agent
 go build apps/config-generator
---
+++ src/datastore/timeserises.go
@@ -0,0 +1,271 @@
+package datastore
+
+import (
+	"bytes"
+	"code.google.com/p/goprotobuf/proto"
+	log "code.google.com/p/log4go"
+	"encoding/binary"
+	"fmt"
+	"github.com/jmhodges/levigo"
+	"strings"
+	"time"
+)
+
+const (
+	KILOBYTES = 1024
+	MEGABYTES = 1024 * KILOBYTES
+)
+
+type TimeseriesDatastore struct {
+	db           *levigo.DB
+	writeOptions *levigo.WriteOptions
+}
+
+func NewTimeseriesDatastore(dir string) (*TimeseriesDatastore, error) {
+	opts := levigo.NewOptions()
+	opts.SetCache(levigo.NewLRUCache(100 * MEGABYTES))
+	opts.SetCreateIfMissing(true)
+	opts.SetBlockSize(256 * KILOBYTES)
+	db, err := levigo.Open(dir, opts)
+	if err != nil {
+		return nil, err
+	}
+	writeOptions := levigo.NewWriteOptions()
+
+	// this initializes the ends of the keyspace so seeks don't mess with us.
+	db.Put(writeOptions, []byte("9999"), []byte(""))
+	db.Put(writeOptions, []byte("0000"), []byte(""))
+	db.Put(writeOptions, []byte("aaaa"), []byte(""))
+	db.Put(writeOptions, []byte("zzzz"), []byte(""))
+	db.Put(writeOptions, []byte("AAAA"), []byte(""))
+	db.Put(writeOptions, []byte("ZZZZ"), []byte(""))
+	db.Put(writeOptions, []byte("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"), []byte(""))
+	db.Put(writeOptions, []byte("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"), []byte(""))
+
+	datastore := &TimeseriesDatastore{
+		db:           db,
+		writeOptions: writeOptions,
+	}
+	return datastore, nil
+}
+
+func (self *TimeseriesDatastore) Close() {
+	if self.db != nil {
+		self.db.Close()
+	}
+	self.db = nil
+}
+
+func (self *TimeseriesDatastore) ReadPoint(database string, series string, id string) (*Point, error) {
+	ro := levigo.NewReadOptions()
+	defer ro.Close()
+	key := fmt.Sprintf("%s~t~%s~%s", database, series, id)
+	data, err := self.db.Get(ro, []byte(key))
+	if err != nil {
+		return nil, err
+	}
+	if data != nil {
+		point := &Point{}
+		err := proto.Unmarshal(data, point)
+		if err != nil {
+			return nil, err
+		}
+		return point, nil
+	}
+	return nil, nil
+}
+
+func (self *TimeseriesDatastore) GetDimensionValue(point *Point, dimensionName *string) (value *string, hasDimension bool) {
+	var dim *Dimension
+	for _, d := range point.Dimensions {
+		if *d.Name == *dimensionName {
+			dim = d
+			break
+		}
+	}
+	if dim != nil {
+		return dim.Value, true
+	}
+	d := ""
+	return &d, false
+}
+
+type GetParams struct {
+	database          string
+	timeSeries        string
+	startTime         int64
+	endTime           int64
+	limit             int64
+	includeContext    bool
+	includeDimensions bool
+	includeIds        bool
+	filter            map[string]string
+	notFilter         map[string]string
+}
+
+func (self *TimeseriesDatastore) ReadSeries(params *GetParams, yield func(*Point) error) error {
+	params.endTime = params.endTime + 1
+	ro := levigo.NewReadOptions()
+	it := self.db.NewIterator(ro)
+	defer it.Close()
+	defer ro.Close()
+
+	beginningKey := fmt.Sprintf("%s~t~%s~", params.database, params.timeSeries)
+	key := fmt.Sprintf("%s~t~%s~%d_", params.database, params.timeSeries, params.endTime)
+
+	it.Seek([]byte(key))
+	if it.Valid() {
+		it.Prev()
+	} else {
+		log.Info("GET_POINTS: first seek wasn't valid")
+	}
+
+	shouldFilter := params.filter != nil && (len(params.filter) > 0 || len(params.notFilter) > 0)
+	for it = it; it.Valid() && params.limit > 0; it.Prev() {
+		pointKey := string(it.Key())
+		if !strings.HasPrefix(pointKey, beginningKey) {
+			break
+		}
+		newPoint := &Point{}
+		err := proto.Unmarshal(it.Value(), newPoint)
+		if err != nil {
+			return err
+		}
+		if *newPoint.Time < params.startTime {
+			break
+		}
+		if shouldFilter {
+			matches := true
+			for dimensionName, expectedValue := range params.filter {
+				val, hasDimension := self.GetDimensionValue(newPoint, &dimensionName)
+				if !hasDimension {
+					matches = false
+				} else if *val != expectedValue {
+					matches = false
+				}
+				if !matches {
+					break
+				}
+			}
+			for dimensionName, expectedValue := range params.notFilter {
+				val, _ := self.GetDimensionValue(newPoint, &dimensionName)
+				if *val == expectedValue {
+					matches = false
+				}
+			}
+
+			if matches {
+				params.limit--
+				if err := yield(newPoint); err != nil {
+					return err
+				}
+			}
+		} else {
+			params.limit--
+			if err := yield(newPoint); err != nil {
+				return err
+			}
+		}
+	}
+	return nil
+}
+
+func (self *TimeseriesDatastore) updateIndex(database, timeseries string, timestamp time.Time) error {
+	// index key
+	key := fmt.Sprintf("%s~i~%s",
+		database,
+		timeseries,
+	)
+
+	ro := levigo.NewReadOptions()
+	defer ro.Close()
+
+	_value, err := self.db.Get(ro, []byte(key))
+	if err != nil {
+		return err
+	}
+
+	var value int64
+	err = binary.Read(bytes.NewReader(_value), binary.LittleEndian, &value)
+	if err != nil {
+		return err
+	}
+
+	lastUpdate := time.Unix(value, 0)
+	if lastUpdate.Sub(timestamp) > 5*time.Minute {
+		buffer := bytes.NewBuffer(nil)
+		err = binary.Write(buffer, binary.LittleEndian, timestamp.Unix())
+		if err != nil {
+			return err
+		}
+		_value = buffer.Bytes()
+		err = self.db.Put(self.writeOptions, []byte(key), _value)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (self *TimeseriesDatastore) writePoints(database string, timeseries string, time time.Time, points []*Point) error {
+	if err := self.updateIndex(database, timeseries, time); err != nil {
+		return err
+	}
+
+	for _, point := range points {
+		key := fmt.Sprintf("%s~t~%s~%d_%d",
+			database,
+			timeseries,
+			time,
+			point.SequenceNumber,
+		)
+		id := fmt.Sprintf("%d_%d", *point.Time, *point.SequenceNumber)
+		oldPoint, err := self.ReadPoint(database, timeseries, id)
+		if err != nil {
+			return err
+		}
+
+		// update value
+		if *point.Value == float64(0) {
+			point.Value = oldPoint.Value
+		}
+
+		// update context
+		if point.Context == nil {
+			point.Context = oldPoint.Context
+		} else if *point.Context == "" {
+			point.Context = nil
+		}
+
+		// update dimensions
+		newDimensions := make([]*Dimension, 0)
+		for _, dim := range oldPoint.Dimensions {
+			newVal, hasDimension := self.GetDimensionValue(point, dim.Name)
+			if hasDimension {
+				if *newVal != "" {
+					dim.Value = newVal
+					newDimensions = append(newDimensions, dim)
+				}
+			} else {
+				newDimensions = append(newDimensions, dim)
+			}
+		}
+		for _, dim := range point.Dimensions {
+			if _, hasDimension := self.GetDimensionValue(oldPoint, dim.Name); !hasDimension {
+				newDimensions = append(newDimensions, dim)
+			}
+		}
+		point.Dimensions = newDimensions
+
+		data, err := proto.Marshal(point)
+		if err != nil {
+			return err
+		}
+		err = self.db.Put(self.writeOptions, []byte(key), data)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
