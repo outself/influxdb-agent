b2c087e58e7b20ce415c79a9209d1aa1199d0cc8
John Shahid <jvshahid@gmail.com>
2013-09-06 14:50:09 -0400

    add a test for getting data from multiple days.

--- src/datastore/timeseries_test.go
+++ src/datastore/timeseries_test.go
@@ -29,13 +29,12 @@ func (self *TimeseriesDatastoreSuite) TearDownTest(c *C) {
 	}
 }
 
-func (self *TimeseriesDatastoreSuite) TestOneDay(c *C) {
+func (self *TimeseriesDatastoreSuite) testDataRetrievalCommon(timestamp1, timestamp2 int64, c *C) {
 	db, err := NewTimeseriesDatastore(self.dbDir)
+	defer db.Close()
 	c.Assert(err, IsNil)
 
-	timestamp1 := time.Now().Add(-5 * time.Second).Unix()
 	value1 := 1.0
-	timestamp2 := time.Now().Unix()
 	value2 := 2.0
 	var sequence uint32 = 1
 
@@ -69,6 +68,17 @@ func (self *TimeseriesDatastoreSuite) TestOneDay(c *C) {
 	c.Assert(points, HasLen, 2)
 	c.Assert(*points[0].Value, Equals, 2.0)
 	c.Assert(*points[1].Value, Equals, 1.0)
+
 }
 
-func (self *TimeseriesDatastoreSuite) TestMultipleDays(c *C) {}
+func (self *TimeseriesDatastoreSuite) TestOneDay(c *C) {
+	timestamp1 := time.Now().Add(-5 * time.Second).Unix()
+	timestamp2 := time.Now().Unix()
+	self.testDataRetrievalCommon(timestamp1, timestamp2, c)
+}
+
+func (self *TimeseriesDatastoreSuite) TestMultipleDays(c *C) {
+	timestamp1 := time.Now().Add(48 * time.Hour).Unix()
+	timestamp2 := time.Now().Add(24 * time.Hour).Unix()
+	self.testDataRetrievalCommon(timestamp1, timestamp2, c)
+}
