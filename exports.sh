b66c2a9d62b1036a8a4478be25b78ae6e49ed667
John Shahid <jvshahid@gmail.com>
2013-09-11 22:26:11 -0400

    fix the exports.

--- exports.sh
+++ exports.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-work=$(python -c 'import os, sys;print os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[1])))' $BASH_SOURCE)
+work=$(python -c 'import os, sys;print os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[1])))' $0)
 export GOPATH=$work/
