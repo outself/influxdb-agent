4fba1e6c32976b510e24617ce22dbf15e59db53a
John Shahid <jvshahid@gmail.com>
2013-09-16 11:03:10 -0400

    add an uninstall script.

---
+++ scripts/uninstall.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e
+
+# finally restart the agent
+service anomalous-agent stop
+
+id anomalous >/dev/null && (echo "Removing 'anomalous' user" && userdel anomalous)
+
+anomalous_dir=/data/anomalous-agent
+anomalous_conf=/etc/anomalous-agent
+
+rm -rf $anomalous_dir
+rm -rf $anomalous_conf
+rm -f /usr/bin/anomalous-agent
+rm -f /usr/bin/anomalous-agent-daemon
+rm -f /usr/bin/anomalous-config-generator
+rm -f /usr/bin/anomalous-sudoers-generator
+
+if which update-rc.d > /dev/null 2>&1 ; then
+    update-rc.d -f anomalous-agent remove
+else
+    chkconfig --del anomalous-agent
+fi
+
+rm /etc/init.d/anomalous-agent
