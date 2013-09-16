15b1b07e904641ed73f2e4bae768e4d4f181d72f
John Shahid <jvshahid@gmail.com>
2013-09-16 12:01:20 -0400

    change the default sleep to 10s.

--- src/apps/config-generator/generator.go
+++ src/apps/config-generator/generator.go
@@ -54,7 +54,7 @@ percentiles:						# the percentiles that will be calculated and sent to Errplane
 flush-interval: 10s			# the rollup interval
 udp-addr: :8127					# the udp port on which the aggregator will listen
 
-sleep: 1m                                     # frequency of sampling (accepted suffix, s for seconds, m for minutes and h for hours)
+sleep: 10s                                    # frequency of sampling (accepted suffix, s for seconds, m for minutes and h for hours)
 proxy:                                        # proxy to use when making http requests (e.g. https://201.20.177.185:8080/)
 log-file: /data/anomalous-agent/shared/log.txt # the log file of the agent
 log-level: info                               # debug, info, warn, error
