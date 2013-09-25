774456491f4ec48b2c55441298ae8a19376089c8
John Shahid <jvshahid@gmail.com>
2013-09-25 11:49:58 -0400

    make the uninstall script work on centos.

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -3,12 +3,18 @@
 set -e
 
 # stop the agent
-service anomalous-agent stop
+/etc/init.d/anomalous-agent stop
 
 id anomalous >/dev/null && (echo "Removing 'anomalous' user" && userdel -f anomalous)
 
 anomalous_dir=/data/anomalous-agent
 anomalous_conf=/etc/anomalous-agent
+
+if which update-rc.d > /dev/null 2>&1 ; then
+    update-rc.d -f anomalous-agent remove
+else
+    chkconfig --del anomalous-agent
+fi
 
 rm -rf $anomalous_dir
 rm -rf $anomalous_conf
@@ -16,11 +22,5 @@ rm -f /usr/bin/anomalous-agent
 rm -f /usr/bin/anomalous-agent-daemon
 rm -f /usr/bin/anomalous-config-generator
 rm -f /usr/bin/anomalous-sudoers-generator
-
-if which update-rc.d > /dev/null 2>&1 ; then
-    update-rc.d -f anomalous-agent remove
-else
-    chkconfig --del anomalous-agent
-fi
 
 rm /etc/init.d/anomalous-agent
