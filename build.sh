3a3bd9f7469bc9629be8e63f9fb0ced354060f8a
John Shahid <jvshahid@gmail.com>
2013-09-25 11:44:14 -0400

    use the old glibc memcpy and not include the stdc++ in the generated binary.

--- build.sh
+++ build.sh
@@ -2,6 +2,9 @@
 
 . exports.sh
 
+current_dir=$(readlink -f $(dirname $0))
+snappy_patch=$current_dir/leveldb-patches/0001-use-the-old-glibc-memcpy-snappy.patch
+leveldb_patch=$current_dir/leveldb-patches/0001-use-the-old-glibc-memcpy-leveldb.patch
 leveldb_version=1.12.0
 snappy_version=1.1.0
 
@@ -18,6 +21,8 @@ if [ ! -d $snappy_dir -o ! -e $snappy_dir/$snappy_file -o ! -e $snappy_dir/.libs
     pushd $snappy_dir
     wget https://snappy.googlecode.com/files/$snappy_file
     tar --strip-components=1 -xvzf $snappy_file
+    # apply the path to use the old memcpy and avoid any references to the GLIBC_2.14
+    patch -p1 < $snappy_patch || (echo "Cannot patch this version of snappy" && exit 1)
     ./configure
     make
     popd
@@ -31,6 +36,8 @@ if [ ! -d $leveldb_dir -o ! -e $leveldb_dir/$leveldb_file -o ! -e $leveldb_dir/l
     pushd $leveldb_dir
     wget https://leveldb.googlecode.com/files/$leveldb_file
     tar --strip-components=1 -xvzf $leveldb_file
+    # apply the path to use the old memcpy and avoid any references to the GLIBC_2.14
+    patch -p1 < $leveldb_patch || (echo "Cannot patch this version of leveldb" && exit 1)
     CXXFLAGS="-I/tmp/snappy" LDFLAGS="-L/tmp/snappy/.libs" make
     popd
 fi
@@ -39,8 +46,12 @@ stdcxx_location=`g++ -print-file-name=libstdc++.a`
 
 git submodule update --init
 
+pushd src/github.com/jmhodges/levigo/
+find . -name \*.go | xargs sed -i 's/\/\/ #cgo LDFLAGS: -lleveldb\|#cgo LDFLAGS: -lleveldb//g'
+popd
+
 export CGO_CFLAGS="-I$leveldb_dir/include"
-export CGO_LDFLAGS="$leveldb_dir/libleveldb.a $snappy_dir/.libs/libsnappy.a $stdcxx_location"
+export CGO_LDFLAGS="-v $leveldb_dir/libleveldb.a $snappy_dir/.libs/libsnappy.a -lstdc++"
 
 go get $build_args github.com/errplane/errplane-go \
     github.com/errplane/gosigar \
