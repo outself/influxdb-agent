e7a0edb3edb7028252872e44753fab84fc955caf
John Shahid <jvshahid@gmail.com>
2013-09-26 10:59:23 -0400

    use different build directories for the agent.

--- exports.sh
+++ exports.sh
@@ -2,7 +2,7 @@
 
 work=$(python -c 'import os, sys;print os.path.abspath(os.path.dirname(os.path.realpath(sys.argv[1])))' $0)
 export GOPATH=$work/
-leveldb_dir=/tmp/leveldb
-snappy_dir=/tmp/snappy
+leveldb_dir=/tmp/leveldb.agent
+snappy_dir=/tmp/snappy.agent
 export CGO_CFLAGS="-I$leveldb_dir/include"
 export CGO_LDFLAGS="$leveldb_dir/libleveldb.a $snappy_dir/.libs/libsnappy.a -lstdc++"
